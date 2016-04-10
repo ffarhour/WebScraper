daf5449698988fa9d03d7decc0ce1c8d4ebb1091
ffarhour <f.farhour@gmail.com>
2016-04-10 02:03:34 -0700

    Based on Rachael's requirements, set up basic part of xml tags. Can now write the author's name from StackOverflow page to xml file.

--- scrape.py
+++ scrape.py
@@ -1,7 +1,34 @@
 from lxml import html
+from lxml import etree
+#from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+#from elementtree.ElementTree import Element, SubElement,tostring
+#from xml.etree import ElementTree as ET
 import requests
 
-page = requests.get('http://stackoverflow.com/questions/8692/how-to-use-xpath-in-python')
+def saveToFile(xml, filename):
+	f = open(filename+".xml", 'w')
+	f.write(xml)
+	f.close()
+
+
+
+page = requests.get('http://stackoverflow.com/questions/138175/dotnetnuke-vulnerabilities')
 tree = html.fromstring(page.content)
 content=tree.xpath('//title/text()')
 print(content)
+
+
+
+# create basic POS XML structure
+TEI = etree.Element('TEI')
+TEI.set('xmlns','http://www.tei-c.org/ns/1.0')
+teiHeader = etree.SubElement(TEI,'teiHeader')
+fileDesc = etree.SubElement(teiHeader,'fileDesc')
+titleStmt = etree.SubElement(fileDesc, 'titleStmt')
+title = etree.SubElement(titleStmt,'title')
+title.text = content[0]
+#author = etree.SubElement(titleStmt,tree.xpath(".//div[@class='user-details']/text()")[0])
+
+# pretty string
+s = etree.tostring(TEI,pretty_print=True)
+saveToFile(s,"test")
