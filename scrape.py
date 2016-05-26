80630184d51982c267db8c0ccb15bcc82234e2f4
ffarhour <f.farhour@gmail.com>
2016-05-26 03:08:11 -0700

    made some changes, but have not solved the escape character problem

--- scrape.py
+++ scrape.py
@@ -42,6 +42,39 @@ def getHtml(url):
     return tree
 
 
+
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+def unescape(text):
+    # import re, htmlentitydefs
+    # def fixup(m):
+    #     text = m.group(0)
+    #     if text[:2] == "&#":
+    #         # character reference
+    #         try:
+    #             if text[:3] == "&#x":
+    #                 return unichr(int(text[3:-1], 16))
+    #             else:
+    #                 return unichr(int(text[2:-1]))
+    #         except ValueError:
+    #             pass
+    #     else:
+    #         # named entity
+    #         try:
+    #             text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+    #         except KeyError:
+    #             pass
+    #     return text # leave as is
+    # return re.sub("&#?\w+;", fixup, text)
+    from html import unescape
+    #h = HTMLParser.HTMLParser()
+    print(unescape(text))
+    return unescape(text)
+
 #fetch html page
 url = input("Please enter the url to the site: \n") or "http://stackoverflow.com/questions/138175/dotnetnuke-vulnerabilities"
 #parse the domain url of the website
@@ -220,8 +253,8 @@ for i in tree.xpath(".//div[@id='answers']//div[@class='answer accepted-answer' 
     ######
     post.text = ""
     for j in i.xpath(".//td[@class='answercell']//div[@class='post-text']/*"): #loop to get full post text
-        print(etree.tostring(j,encoding='utf-8',method="html").decode('utf-8','ignore'))
-        post.text += etree.tostring(j,encoding='utf-8',method="html").decode('utf-8','ignore')
+        print(etree.tostring(j,encoding='ascii',method="html").decode('utf-8','ignore'))
+        post.text += unescape(etree.tostring(j,encoding='ascii',method="html").decode('utf-8','ignore'))
         # print(j.xpath("descendant-or-self::text()"))
         # for k in j.xpath("descendant-or-self::text()"): #decendant-or-self gets all text, even with br elements present #change text to node(), then iterate through it
         #     #for l in k.xpath("text()"):
@@ -258,5 +291,10 @@ for i in tree.xpath(".//div[@id='answers']//div[@class='answer accepted-answer' 
         p.text =" ".join(j.xpath(".//span[@class='comment-copy']/descendant-or-self::*/text()[normalize-space()]"))
 
 # pretty string
-s = etree.tostring(TEI,pretty_print=True,encoding='UTF-8')
+s = etree.tostring(TEI,pretty_print=True,encoding='utf-8')
+# s_xml = etree.fromstring(s)
+# from lxml.html import clean
+# s_xml = clean_html(s_xml)
+# result = lxml.html.tostring(s_xml, encoding="ascii")
+# print(result)
 saveToFile(s,"post")
